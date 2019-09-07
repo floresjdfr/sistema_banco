723b99af2ae18f8bb688836da0d0bd2826899457
floresjdfr <49382573+floresjdfr@users.noreply.github.com>
2019-09-07 16:32:21 -0600

    Iniciando proyecto

---
+++ SistemaBanco/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Jose David
+ *
+ * Created on September 7, 2019, 4:28 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+

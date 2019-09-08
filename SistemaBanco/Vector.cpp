76e5cab312f4aa995308d5f889ec11dd210345d9
Giancarlo Chaves Matarrita <38262260+ThaSleepinGiank@users.noreply.github.com>
2019-09-08 17:36:20 -0600

    Update Vector.cpp

--- SistemaBanco/Vector.cpp
+++ SistemaBanco/Vector.cpp
@@ -7,34 +7,41 @@
 
 #include "Vector.h"
 
+template <class T>
 Vector::Vector(){
     vector = new T*[this->getCantidad()];
     this->setCantidad(0);
 }
 
+template <class T>
 Vector::Vector(int tam){
     vector = new T*[this->getCantidad()];
     this->setCantidad(tam);
 }
 
+template <class T>
 Vector::~Vector(){
     for(int i=0; i<this->getCantidad(); i++)
         delete vector[i];
     delete [] vector;
 }
 
+template <class T>
 void Vector::setCantidad(int can){
     this->cantidad = can;
 }
 
+template <class T>
 int Vector::getCantidad(){
     return this->cantidad;
 }
 
+template <class T>
 void Vector::agregar(T* elem){
     vector[cantidad++] = elem;
 }
 
+template <class T>
 ostream& Vector::operator <<(ostream& salida, T<Vector>& this){
     
 }

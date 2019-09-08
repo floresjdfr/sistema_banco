b1d4d38658e76426125f69202e4c30932e7b332d
Giancarlo Chaves Matarrita <38262260+ThaSleepinGiank@users.noreply.github.com>
2019-09-08 16:19:38 -0600

    Update Persona.h

--- SistemaBanco/Persona.h
+++ SistemaBanco/Persona.h
@@ -5,40 +5,12 @@
  * Created on September 7, 2019, 4:33 PM
  */
 
-#include "Cliente.h"
-
 #ifndef PERSONA_H
 #define	PERSONA_H
 
-class Persona : public Cliente {
+class Persona {
 public:
-    Persona(string, string, string, int, float);
-    Persona();
-    
-    //Sets
-    void setNombre(string);
-    void setIdentificacion(string);
-    void setCorreoElectronico(string);
-    void setEdad(int);
-    void setSalario(float);
-    
-    //Gets
-    string getNombre() const;
-    string getIdentificacion() const;
-    string getCorreoElectronico() const;
-    int getEdad() const;
-    float getSalario() const;
-    
-    //Otros metodos
-    string toString() const;
-    
-    
-private:
-    string nombre;
-    string identificacion;
-    string correoElectronico;
-    int edad;
-    float salario;
+    virtual std::string toString() const = 0;
 };
 
 #endif	/* PERSONA_H */

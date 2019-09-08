caee1d93e60d5495d0b2d0a797899ba8560b9c4a
Giancarlo Chaves Matarrita <38262260+ThaSleepinGiank@users.noreply.github.com>
2019-09-08 17:35:35 -0600

    Update Lista.cpp

--- SistemaBanco/Lista.cpp
+++ SistemaBanco/Lista.cpp
@@ -7,28 +7,34 @@
 
 #include "Lista.h"
 
+template <class T>
 Lista::Lista(){
     primero = NULL;
     actual = NULL;
 }
 
+template <class T>
 Lista::~Lista(){
     delete primero;
     delete actual;
 }
 
+template <class T>
 Nodo* Lista::getPrimero(){
     return primero;
 }
 
+template <class T>
 Nodo* Lista::getActual(){
     return actual;
 }
 
+template <class T>
 Nodo* Lista::getSiguiente(Nodo* nodoTemp){
     return nodoTemp->siguiente;
 }
 
+template <class T>
 void Lista::agregar(T* nuevo){
     T<Nodo>* aux = nuevo;
     if(primero != NULL){
@@ -38,6 +44,7 @@ void Lista::agregar(T* nuevo){
     primero = actual = aux;
 }
 
+template <class T>
 ostream& Lista::operator <<(ostream& salida, T<Lista>& this){
     
 }

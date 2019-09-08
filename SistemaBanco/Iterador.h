12d7400ca00e84df34de0e4e7c98a537011539f1
Giancarlo Chaves Matarrita <38262260+ThaSleepinGiank@users.noreply.github.com>
2019-09-08 17:03:30 -0600

    Actualizado class Iterador

--- SistemaBanco/Iterador.h
+++ SistemaBanco/Iterador.h
@@ -8,23 +8,49 @@
 #ifndef ITERADOR_H
 #define	ITERADOR_H
 
-#include "Nodo.h"
-
 template <class T>
 class Iterador { 
 public:
-    Iterador(Nodo<T>*);
-    bool masElementos();
-    void siguienteElemento(); //Siguiente nodo
-    void primerElemento(); //Primer nodo
+    Iterador(T**, int);
+    bool hayMasElementos();
+    void siguiente(); //Siguiente nodo
+    void primero(); //Primer nodo
     T* elementoActual();
 private:
-    Nodo<T*> actual;
-    Nodo<T*> primero;
-    
-    
-
+    T* *elementos;
+    int tamano;
+    int cursor;
 };
+
+template <class T>
+Iterador::Iterador(T* *elem, int tam){
+    elementos = elem;
+    tamano = tam;
+    cursor = 0;
+}
+
+template <class T>
+bool Iterador::hayMasElementos(){
+    return (elementos[cursor] != std::NULL && cursor < tamano);
+}
+
+template <class T>
+void Iterador::siguiente(){
+    cursor++;
+}
+
+template <class T>
+void Iterador::primero(){
+    curosor = 0;
+}
+
+template <class T>
+T* Iterador::elementoActual(){
+    T* aux;
+    if(hayMasElementos())
+        aux = elementos[cursor];
+    return aux;
+}
 
 #endif	/* ITERADOR_H */
 

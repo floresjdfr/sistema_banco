45ad017325b1559950d032b7b171f6fb27840175
floresjdfr <49382573+floresjdfr@users.noreply.github.com>
2019-09-09 17:37:59 -0600

    Clase abstracta "Tarjeta" agregada

---
+++ SistemaBanco/Tarjeta.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Tarjeta.h
+ * Author: Jose David
+ *
+ * Created on September 9, 2019, 5:36 PM
+ */
+
+#ifndef TARJETA_H
+#define	TARJETA_H
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class Tarjeta {
+public:
+    virtual string toString() = 0;
+
+};
+
+#endif	/* TARJETA_H */
+

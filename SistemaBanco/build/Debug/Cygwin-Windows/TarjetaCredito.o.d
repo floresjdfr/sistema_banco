4734a4048920d5698c2be5720b6d80932bf9cdfc
floresjdfr <49382573+floresjdfr@users.noreply.github.com>
2019-09-19 00:51:24 -0600

    ignore deleted

---
+++ SistemaBanco/build/Debug/Cygwin-Windows/TarjetaCredito.o.d
@@ -0,0 +1,12 @@
+build/Debug/Cygwin-Windows/TarjetaCredito.o: TarjetaCredito.cpp \
+ TarjetaCredito.h Tarjeta.h Fecha.h Compra.h Transaccion.h
+
+TarjetaCredito.h:
+
+Tarjeta.h:
+
+Fecha.h:
+
+Compra.h:
+
+Transaccion.h:
